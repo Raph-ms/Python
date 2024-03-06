0b07baec42cc4a0210c03b15d2a539aff9a29bea
Raphael Martins <raphaelmartinsdasilva222@hotmail.com>
2024-03-06 15:30:12 -0300

    Create ex01.py

---
+++ aula02/ex01.py
@@ -0,0 +1,9 @@
+print("Seja bem vindo á calculadora!")
+num01 = int(input("Digite o primeiro numero:"))
+num02 = int(input("Digite o segundo numero:"))
+
+print("O resultado da adição:", num01 + num02)
+print("O resultado da subtração", num01 - num02)
+print("O resultado da multiplicação:", num01 * num02)
+print("O resultado da divisão:", num01 / num02)
+
